d9f77f782243dd52801cf1d0d68d7cf15cdb6941
elmir14 <zhumabaevelmir@gmail.com>
2023-08-28 17:55:44 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,38 @@
+//1ex
+// let a = [1, 3, 5, 8];
+// let b = [1, 3, 5, 8];
+
+// function equal(x, y) {
+//   return x.length === y.length &&
+//          x.every((element, index) => y[index] === element);
+// }
+
+// console.log(equal(a, b));
+
+//2ex
+// function pow(number , degree){
+//     return  Math.pow(number , degree)
+// }
+// const result = pow(2,3);
+// console.log(result);
+
+//3ex
+// var x = "frontend"
+// var empty = " "
+// console.log( "frontend занимает " + x.length + " кодовых значений" );
+
+// console.log("пустая строка имеет длину, равную  " + empty.length );
+
+//1dop
+// let chars = [ 1,1,1,2,3,5,5,7,8,3];
+// let uniguechars = [...new Set (chars)];
+
+// console.log(uniguechars);
+
+
+//2dop
+// const sms = (phone,text) => {
+//     alert(phone, text)
+// }
+
+// sms(+996500200, "elmir");
